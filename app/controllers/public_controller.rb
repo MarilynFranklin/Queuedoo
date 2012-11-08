2dbe526a7da0d2209e1dd08cd60416bc7d4354a2
Marilyn <marilyn.j.franklin@gmail.com>
2012-11-08 16:49:22 -0600

    Add rspec

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,5 @@
+class PublicController < ApplicationController
+  def index
+    render text: "hello, world"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 QueueApp::Application.routes.draw do
+  root :to => 'public#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
