9bda7dceec7222bc425a057dd7399974ce930dec
Marilyn <marilyn.j.franklin@gmail.com>
2012-11-13 09:32:17 -0600

    refactored queuer controller

--- app/controllers/queuers_controller.rb
+++ app/controllers/queuers_controller.rb
@@ -1,22 +1,13 @@
 class QueuersController < ApplicationController
 
-  def edit
-    @queuer = Queuer.find(params[:id])
-    @line = Line.find(params[:line_id])
-  end
-
-  def show
-    @queuer = Queuer.find(params[:id])
-  end
+  before_filter :lookup_queuer
+  before_filter :lookup_line, except: [:show]
 
   def create
-    @line = Line.find(params[:line_id])
-    @queuer = Queuer.new(params[:queuer])
     @queuer.line = @line
 
     if @queuer.save
-      redirect_to @line
-      flash[:notice] = "You have added someone to the line"
+      redirect_to @line, notice: "You have added someone to the line"
     else
       flash[:error] = @queuer.errors.full_messages.join(". ")
       render 'lines/show'
@@ -24,20 +15,30 @@ class QueuersController < ApplicationController
   end
 
   def processed
-    @line = Line.find(params[:line_id])
-    @queuer = Queuer.find(params[:id])
     @queuer.update_attributes(processed: true)
     redirect_to @line, notice: "Processed"
   end
 
   def update
-    @queuer = Queuer.find(params[:id])
-    @line = Line.find(params[:line_id])
     if @queuer.update_attributes(params[:queuer])
       redirect_to [@line, @queuer], notice: "Profile has been updated"
     else
       flash[:error] = @queuer.errors.full_messages.join
       render :edit
     end
+  end
+
+  protected
+
+  def lookup_queuer
+    if params[:id]
+      @queuer = Queuer.find(params[:id])
+    else
+      @queuer = Queuer.new(params[:queuer])
+    end
+  end
+
+  def lookup_line
+    @line = Line.find(params[:line_id])
   end
 end
