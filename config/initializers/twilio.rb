6a8e50d061d2116a081af2ea0b02e0f5b97dd334
Marilyn <marilyn.j.franklin@gmail.com>
2012-11-20 11:08:33 -0600

    Added twilio configuration for production

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,2 +1,9 @@
-path = File.join(Rails.root, "config/twilio.yml")
-TWILIO_CONFIG = YAML.load(File.read(path))[Rails.env] || {'sid' => '', 'from' => '', 'token' => ''}
+unless Rails.env.production?
+  path = File.join(Rails.root, "config/twilio.yml")
+  TWILIO_CONFIG = YAML.load(File.read(path))[Rails.env] || {'sid' => '', 'from' => '', 'token' => ''}
+else
+  TWILIO_CONFIG = {}
+  TWILIO_CONFIG['sid'] = ENV['TWILIO_SID']
+  TWILIO_CONFIG['from'] = ENV['TWILIO_FROM']
+  TWILIO_CONFIG['token'] = ENV['TWILIO_TOKEN']
+end
